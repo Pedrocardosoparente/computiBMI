249c54e993875f76c14f90096976316f5ac01d44
Aluno <Aluno@10.85.198.36>
2024-06-05 16:37:33 -0300

    computi

---
+++ src/computibmi/ComputeBMI.java
@@ -0,0 +1,15 @@
+
+
+
+public class ComputeBMI {
+    public static void main(String[] args)
+	{
+            double x = 132.5;
+            double y = 62.5;
+            double m = y * y;
+            double v = x/m;
+            double f = Math.ceil(v * 703);
+            System.out.println(f);
+        
+        }
+}
